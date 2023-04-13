a9de88fb55ea3d7d537993fac0ffd0db43683d17
Eric Seader <45432901+ejseader@users.noreply.github.com>
2023-04-13 14:52:32 -0400

    fixed schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,31 +6,25 @@ USE employees_db;
 CREATE TABLE departments (
   dept_id INT AUTO_INCREMENT,
   name VARCHAR(30),
-  PRIMARY KEY (id)
+  PRIMARY KEY (dept_id)
 );
 
 CREATE TABLE roles (
-  role_id INT AUTO_INCREMENT,
+  role_id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
   dept_id INT,
-  PRIMARY KEY (role_id)
-  FOREIGN KEY (dept_id)
-  REFERENCES departments(dept_id)
-  ON DELETE SET NULL
+  FOREIGN KEY (dept_id) REFERENCES departments(dept_id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-  emp_id INT AUTO_INCREMENT,
+  emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  title VARCHAR(30),
+  role_id INT,
   manager_id INT DEFAULT NULL,
-  CONSTRAINT id PRIMARY KEY (emp_id)
-  CONSTRAINT manager_id FOREIGN KEY (manager_id)
-  FOREIGN KEY (title)
-  REFERENCES roles(title)
-  REFERENCES employees(emp_id)
+  FOREIGN KEY (role_id) REFERENCES roles(role_id),
+  FOREIGN KEY (manager_id) REFERENCES roles(role_id)
 );
 
 -- SELECT
